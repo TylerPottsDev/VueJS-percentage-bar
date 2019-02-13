34801731dde387965d2da2a48b51f1b2844e0fca
Tyler Potts <tylerpotts@Tylers-MacBook-Pro-2.local>
2019-02-13 00:31:03 +0000

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Post title="Post Title" />
+		<Post title="Post Title 2" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import Post from './components/Post';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'app',
+		components: {
+			Post
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+	body
+		margin: 0
+		padding: 0
+		background:
+			color: #F3F3F3
+		#app
+			padding: 25px
 </style>
